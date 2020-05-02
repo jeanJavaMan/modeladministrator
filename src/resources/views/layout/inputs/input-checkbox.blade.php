f0f6b58e51d1db30617c3b57952e2ea26806a117
Jeanderson S.lopes <jeandersonsilvalopes@gmail.com>
2020-05-02 18:18:22 -0300

    melhorado checkbox

--- src/resources/views/layout/inputs/input-checkbox.blade.php
+++ src/resources/views/layout/inputs/input-checkbox.blade.php
@@ -1,14 +1,15 @@
 @php
     /**@var \Jeanderson\modeladministrator\Models\Element $element*/
 @endphp
-<div class="custom-control custom-checkbox">
-    <input class="custom-control-input {{$element->class_field}}" type="{{$element->type_input}}"
-           id="{{$element->fillable_var}}" name="{{$element->fillable_var}}"
-           {{old($element->fillable_var) !== null ? "checked":""}} @foreach(explode(";",$element->attributes) as $attribute) {!! $attribute !!}  @endforeach>
-    <label for="{{$element->fillable_var}}" class="custom-control-label">{{$element->label}}</label>
-</div>
+<label for="{{$element->fillable_var}}">{{$element->label}}:</label>
+<select class="js-select form-control" id="{{$element->fillable_var}}"
+        name="{{$element->fillable_var}}" @foreach(explode(";",$element->attributes) as $attribute) {!! $attribute !!} @endforeach>
+    <option {{old($element->fillable_var,"1") == "1" ? "selected":""}} value="1">{{__("modeladminlang::default.yes")}}</option>
+    <option {{old($element->fillable_var,"1") == "0" ? "selected":""}} value="0">{{__("modeladminlang::default.no")}}</option>
+</select>
 @if($errors ?? false)
     @if($errors->has($element->fillable_var))
-        <div class="alert-danger" style="border-radius: .25rem"><p style="margin-left: 5px">{{ $errors->first($element->fillable_var)}}</p></div>
+        <div class="alert-danger" style="border-radius: .25rem"><p
+                style="margin-left: 5px">{{ $errors->first($element->fillable_var)}}</p></div>
     @endif
 @endif
